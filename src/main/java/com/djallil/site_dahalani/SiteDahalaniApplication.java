9d0bd3dffde57ca9f3b2d37c92c78205316aad01
Djallil94 <djallil.ahamada@efrei.net>
2026-04-09 00:35:07 +0200

    Premier commit

---
+++ src/main/java/com/djallil/site_dahalani/SiteDahalaniApplication.java
@@ -0,0 +1,13 @@
+package com.djallil.site_dahalani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteDahalaniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteDahalaniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/djallil/site_dahalani/SiteDahalaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djallil.site_dahalani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteDahalaniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
