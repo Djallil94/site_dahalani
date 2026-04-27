1d9c947d3dbda23aaa1293e0212c41cab334ddda
Djallil AHAMADA <ahamadadjallil@gmail.com>
2026-04-27 22:41:23 +0200

    Changement du background et de la police

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './pages/accueil/accueil';
+import { ServicesPage } from './pages/services-page/services-page';
+import { Contact } from './pages/contact/contact';
+import {PageExpertise} from './pages/page-expertise/page-expertise';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Accueil },
+  { path: 'services', component: ServicesPage },
+  { path: 'page-expertise', component : PageExpertise},
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: ''}
+];
