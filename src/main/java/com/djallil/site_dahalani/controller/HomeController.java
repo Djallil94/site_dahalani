70505d997b2a19f049e829c3627e3e6eb3ffc2ba
Djallil AHAMADA <ahamadadjallil@gmail.com>
2026-05-04 11:21:30 +0200

    Ajout HomeController

---
+++ src/main/java/com/djallil/site_dahalani/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.djallil.site_dahalani.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API opérationnelle !";
+    }
+}
